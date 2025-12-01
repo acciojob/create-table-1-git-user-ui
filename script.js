769ec706fd6421856327e3096da01f30372fe54d
git-user-ui <pratikpp2302@gmail.com>
2025-12-01 23:09:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 const tble = document.getElementById("sampleTable");
-let i = tble.rows.length +1;  // start counting from existing rows + 1
 
 function insert_Row() {
-
-    // Create a new row
-    const newRow = tble.insertRow()
-
-    // Create two new cells
-    const cell1 = newRow.insertCell(0);
-    const cell2 = newRow.insertCell(1);
-
-    // Set cell content
-    cell1.textContent = `Row${i} cell1`;
-    cell2.textContent = `Row${i} cell2`;
-
-    // Increase counter for next insertion
-    i++;
+    tble.innerHTML += 
+		`
+		<tr><td>new cell1</td> 
+		<td>new cell2</td></tr> 
+		`
 }
